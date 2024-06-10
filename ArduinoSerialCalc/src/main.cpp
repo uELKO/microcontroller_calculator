162eff6cc9008f8a0bafdeb58c5fdb18b8c17b33
Stefan Poß <100678744+uELKO@users.noreply.github.com>
2024-06-10 17:13:45 +0200

    Update main.cpp
    Main structure functional

--- ArduinoSerialCalc/src/main.cpp
+++ ArduinoSerialCalc/src/main.cpp
@@ -8,14 +8,34 @@ void setup()
     g_serialInputBuffer.reserve(200);
 }
 
+int32_t math_add(int16_t x, int16_t y) {
+  return x + y;
+}
+
+int32_t math_subtract(int16_t x, int16_t y) {
+  return x - y;
+}
+
+int32_t math_multiply(int16_t x, int16_t y) {
+  return x * y;
+}
+
+double math_divide(int16_t x, int16_t y) {
+  return x / y;
+}
+
+double math_power(int16_t x, int16_t y) {
+  return pow(x, y);
+}
+
 void processData(String data)
 {
     int32_t tempX, tempY;
     char tempCmd;
 
-    int32_t index1 = data.indexOf(';');
-    int32_t index2 = data.indexOf(';', index1 + 1);
-    int32_t index3 = data.indexOf(';', index2 + 1);
+    int index1 = data.indexOf(';');
+    int index2 = data.indexOf(';', index1 + 1);
+    int index3 = data.indexOf(';', index2 + 1);
 
     if (index1 != -1 && index2 != -1 && index3 != -1)
     {
@@ -23,13 +43,51 @@ void processData(String data)
         tempY = data.substring(index1 + 1, index2).toInt();
         tempCmd = data.charAt(index2 + 1);
 
-        Serial.println(tempX);
-        Serial.println(tempY);
-        Serial.println(tempCmd);
+        int32_t result_i = 0;
+        double result_d = 0;
+
+        switch (tempCmd)
+        {
+        case 'a':
+            result_i = math_add(tempX, tempY);
+            break;
+
+        case 's':
+            result_i = math_subtract(tempX, tempY);
+            break;
+
+        case 'm':
+            result_d = math_multiply(tempX, tempY);
+            break;
+
+        case 'd':
+            result_d = math_divide(tempX, tempY);
+            break;
+
+        case 'p':
+            result_d = math_power(tempX, tempY);
+            break;
+
+        default:
+            break;
+        }
+
+        if(tempCmd == 'a' || tempCmd == 's' || tempCmd == 'm' )
+        {
+            Serial.println(result_i);
+        }
+        else if(tempCmd == 'd' || tempCmd == 'p')
+        {
+            Serial.println(result_d);
+        }
+        else
+        {
+            Serial.println("Error: Calculation operand invalid.");
+        }
     }
     else
     {
-        Serial.println("Fehler beim Parsen der Daten.");
+        Serial.println("Error: Structure of the data package incorrect.");
     }
 }
 
@@ -37,15 +95,15 @@ void loop()
 {
     while (Serial.available())
     {
-        char inChar = (char)Serial.read();
-        if (inChar == '\n')
+        char receivedChar = (char)Serial.read();
+        if (receivedChar == '\n')
         {
             processData(g_serialInputBuffer);
             g_serialInputBuffer = "";
         }
         else
         {
-            g_serialInputBuffer += inChar;
+            g_serialInputBuffer += receivedChar;
         }
     }
 }
